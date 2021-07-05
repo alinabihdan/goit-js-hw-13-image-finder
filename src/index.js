98c2e5f955798a3b15c354ead6f81a81f88329a2
alinabihdan <alina.bigdan1@gmail.com>
2021-07-05 11:34:26 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 import './sass/main.scss';
 import ApiService from './js/apiService.js';
 import cardTpl from './templates/imageCardTpl.hbs';
+// import Isotope from 'isotope-layout';
 // import '@pnotify/core/dist/BrightTheme.css';
 // import '@pnotify/core/dist/PNotify.css';
 // import { defaults, info, success, error } from '@pnotify/core';
 // defaults.mouseReset = false;
 // defaults.delay = 3000;
 
+// const elem = document.querySelector('.grid');
+// const iso = new Isotope(elem, {
+//   // options
+//   itemSelector: '.grid-item',
+//   layoutMode: 'masonry',
+// });
+
 const PicsApi = new ApiService();
 
 const refs = {
   searchForm: document.querySelector('#js-search-form'),
   gallery: document.querySelector('.gallery'),
+  anchor: document.querySelector('.anchor'),
 };
 
 refs.searchForm.addEventListener('submit', onSearch);
@@ -46,4 +55,16 @@ function appendPicsMarkup(pics) {
 
 function clearPicsContainer() {
   refs.gallery.innerHTML = '';
+}
+
+const observer = new IntersectionObserver(observerCallback, {
+  threshold: 0,
+});
+
+observer.observe(refs.anchor);
+
+function observerCallback([entrie], observerRef) {
+  if (!entrie.isIntersecting) return;
+
+  fetchImages();
 }
